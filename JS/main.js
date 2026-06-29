339996eed89829cb610015746cc21b462642aaaa
Lionela Abril Ledesma <lionelaabril.ledesma@estudiantes.unahur.edu.ar>
2026-06-29 20:46:21 -0300

    feat: Agrego modo oscuro con JS

---
+++ JS/main.js
@@ -0,0 +1,10 @@
+const boton = document.getElementById("btn-modo");
+
+boton.addEventListener("click", () => {
+    document.body.classList.toggle("oscuro");
+    if (document.body.classList.contains("oscuro")) {
+        boton.innerHTML = "☀ Modo claro";
+    } else {
+        boton.innerHTML = "🌙 Modo oscuro";
+    }
+});
